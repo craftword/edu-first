0a2fca9f08a7c30ef98d112a1da0f7c574a6b3c8
Abimbola <craftwordltd@gmail.com>
2020-01-25 02:27:18 +0100

    created other models

---
+++ edu-first/Models/Registration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace edu_first.Models
+{
+    public class Registration
+    {
+        public int UserId { get; set; }
+        public int SessionId { get; set; }
+        public int CourseId { get; set; }
+        public int UserId { get; set; }
+    }
+}
