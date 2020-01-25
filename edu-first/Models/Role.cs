e715b8cea5411cce9b2e1a90569d2db2de0b2299
Abimbola <craftwordltd@gmail.com>
2020-01-25 01:52:02 +0100

    created role and user model

--- edu-first/Models/Role.cs
+++ edu-first/Models/Role.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+;
 
 namespace edu_first.Models
 {
-    public class User : IdentityUser
+    public class Role
     {
+        [Required]
+        public string RoleName { get; set; }
 
     }
 }
