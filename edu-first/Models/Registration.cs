eeb9c9d6c542c723374c2bc44be62e5453a848f7
cidospark <cidospark27@gmail.com>
2020-01-28 22:01:31 +0100

    added registration model relationship fileds

--- edu-first/Models/Registration.cs
+++ edu-first/Models/Registration.cs
@@ -10,5 +10,8 @@ namespace edu_first.Models
         public int UserId { get; set; }
         public int SessionId { get; set; }
         public int CourseId { get; set; }
+        public User User { get; set; }
+        public Session Session { get; set; }
+        public Course Course { get; set; }
     }
 }
