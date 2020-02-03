89e627712db8cc5b71dee82946af32b5d31be07a
Abimbola <craftwordltd@gmail.com>
2020-02-03 10:14:23 +0100

    create role views and controller

---
+++ edu-first/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace edu_first.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Role Name is required")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
